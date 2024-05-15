b034fa59ba4efc9709aa00d346782e754618fff6
Bablu-debug <bilalhussain309@gmail.com>
2024-05-15 21:06:55 +0500

    change for vercel

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
 'use strict';
-const server = require('./server')
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  routes = require("./server/routes"),
+  colors = require("colors"),
+  dotenv = require('dotenv'),
+  mongooseInit = require("./server/data_access/index");
 
-server.create();
-server.start();
+let server = express()
+dotenv.config()
+  server.use(bodyParser.json());
+  server.use(bodyParser.urlencoded({ extended: true }));
+
+  server.use(express.json());
+  server.use(express.urlencoded({ extended: true }));
+
+  server.use(express.static("public"));
+
+  // Set up routes
+  routes.init(server);
+  // ================= SET UP ROUTES FOR WEB HOOKS
+
+  // Initiating mongoose connection
+  mongooseInit.initMongooseConnection().then((connected) => {
+    if (connected) {
+      console.log(
+        colors.green(
+          `Mongoose: connection to mongodb successful ✓`
+        ),
+        ":",
+        colors.magenta(` Connection Date: ${new Date()} `)
+      );
+    } else {
+      console.log(colors.red("Mongoose connection was unsuccessful"));
+    }
+  });
+
+  server.listen(process.env.PORT, function () {
+    console.log(
+      `Starting ${process.env.DOMAIN_NAME} Backend Server at: ${process.env.PORT}`
+    );
+  });
+
